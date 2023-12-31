a6e56b040b0ebdaa41a1a3faebe75a445d8080e9
Rizal <rizalbario3@gmail.com>
2023-12-31 08:02:42 +0700

    add userControlforDashboard and Function it

---
+++ Attendance Management System/Model/UserControlCheck/UserControlforDashboard.Designer.cs
@@ -0,0 +1,206 @@
+﻿namespace Attendance_Management_System.Model.UserControlCheck
+{
+    partial class UserControlforDashboard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControlforDashboard));
+            panel3 = new Panel();
+            label5 = new Label();
+            labeltotalRole = new Label();
+            pictureBox3 = new PictureBox();
+            panel2 = new Panel();
+            labeltotalStudent = new Label();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            panel1 = new Panel();
+            labeltotalClass = new Label();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.FromArgb(255, 128, 0);
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(labeltotalRole);
+            panel3.Controls.Add(pictureBox3);
+            panel3.Location = new Point(380, 292);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(250, 144);
+            panel3.TabIndex = 5;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(132, 95);
+            label5.Name = "label5";
+            label5.Size = new Size(26, 20);
+            label5.TabIndex = 2;
+            label5.Text = "{?}";
+            // 
+            // labeltotalRole
+            // 
+            labeltotalRole.AutoSize = true;
+            labeltotalRole.Location = new Point(132, 58);
+            labeltotalRole.Name = "labeltotalRole";
+            labeltotalRole.Size = new Size(39, 20);
+            labeltotalRole.TabIndex = 1;
+            labeltotalRole.Text = "Role";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(14, 16);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(74, 62);
+            pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox3.TabIndex = 0;
+            pictureBox3.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.FromArgb(255, 255, 128);
+            panel2.Controls.Add(labeltotalStudent);
+            panel2.Controls.Add(label4);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Location = new Point(602, 100);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(250, 144);
+            panel2.TabIndex = 4;
+            // 
+            // labeltotalStudent
+            // 
+            labeltotalStudent.AutoSize = true;
+            labeltotalStudent.Location = new Point(132, 95);
+            labeltotalStudent.Name = "labeltotalStudent";
+            labeltotalStudent.Size = new Size(26, 20);
+            labeltotalStudent.TabIndex = 2;
+            labeltotalStudent.Text = "{?}";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(132, 58);
+            label4.Name = "label4";
+            label4.Size = new Size(60, 20);
+            label4.TabIndex = 1;
+            label4.Text = "Student";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(14, 16);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(74, 62);
+            pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = SystemColors.ActiveCaption;
+            panel1.Controls.Add(labeltotalClass);
+            panel1.Controls.Add(label1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(157, 100);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(250, 144);
+            panel1.TabIndex = 3;
+            // 
+            // labeltotalClass
+            // 
+            labeltotalClass.AutoSize = true;
+            labeltotalClass.Location = new Point(132, 95);
+            labeltotalClass.Name = "labeltotalClass";
+            labeltotalClass.Size = new Size(26, 20);
+            labeltotalClass.TabIndex = 2;
+            labeltotalClass.Text = "{?}";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(132, 58);
+            label1.Name = "label1";
+            label1.Size = new Size(42, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Class";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(14, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(74, 62);
+            pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
+            pictureBox1.TabIndex = 0;
+            pictureBox1.TabStop = false;
+            // 
+            // UserControlforDashboard
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(panel3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "UserControlforDashboard";
+            Size = new Size(1009, 537);
+            Load += UserControlforDashboard_Load;
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel3;
+        private Label label5;
+        private Label labeltotalRole;
+        private PictureBox pictureBox3;
+        private Panel panel2;
+        private Label labeltotalStudent;
+        private Label label4;
+        private PictureBox pictureBox2;
+        private Panel panel1;
+        private Label labeltotalClass;
+        private Label label1;
+        private PictureBox pictureBox1;
+    }
+}
--- Attendance Management System/View/FormMain.Designer.cs
+++ Attendance Management System/View/FormMain.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             panelSide = new Panel();
             pictureBoxRegister = new PictureBox();
@@ -43,23 +44,25 @@
             pictureDashboard = new PictureBox();
             buttonDashboard = new Button();
             panel3 = new Panel();
-            panel4 = new Panel();
+            panelSideButton = new Panel();
             label1 = new Label();
             panel1 = new Panel();
             pictureBox1 = new PictureBox();
             panelUp = new Panel();
             pictureBoxExpand = new PictureBox();
             pictureBox2 = new PictureBox();
-            label_welcome = new Label();
+            labelTime = new Label();
             panelCek = new Panel();
             panelExpand = new Panel();
             button2 = new Button();
             button1 = new Button();
             panel2 = new Panel();
-            label4 = new Label();
+            labelRole = new Label();
             label5 = new Label();
-            label3 = new Label();
+            labelUsername = new Label();
             label2 = new Label();
+            timerDateAndTime = new System.Windows.Forms.Timer(components);
+            userControlforDashboard1 = new Model.UserControlCheck.UserControlforDashboard();
             panelSide.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBoxRegister).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBoxReport).BeginInit();
@@ -124,6 +127,7 @@
             buttonRegister.TabIndex = 12;
             buttonRegister.Text = "Register";
             buttonRegister.UseVisualStyleBackColor = false;
+            buttonRegister.Click += buttonRegister_Click;
             // 
             // pictureBoxReport
             // 
@@ -147,6 +151,7 @@
             buttonReport.TabIndex = 10;
             buttonReport.Text = "Report";
             buttonReport.UseVisualStyleBackColor = false;
+            buttonReport.Click += buttonReport_Click;
             // 
             // pictureBoxMember
             // 
@@ -170,6 +175,7 @@
             buttonMember.TabIndex = 8;
             buttonMember.Text = "Member";
             buttonMember.UseVisualStyleBackColor = false;
+            buttonMember.Click += buttonMember_Click;
             // 
             // pictureBoxSession
             // 
@@ -193,6 +199,7 @@
             buttonSession.TabIndex = 6;
             buttonSession.Text = "Session";
             buttonSession.UseVisualStyleBackColor = false;
+            buttonSession.Click += buttonSession_Click;
             // 
             // pictureBoxAttendance
             // 
@@ -216,6 +223,7 @@
             buttonAttendance.TabIndex = 4;
             buttonAttendance.Text = "Attendance";
             buttonAttendance.UseVisualStyleBackColor = false;
+            buttonAttendance.Click += buttonAttendance_Click;
             // 
             // pictureDashboard
             // 
@@ -239,23 +247,26 @@
             buttonDashboard.TabIndex = 2;
             buttonDashboard.Text = "Dasboard";
             buttonDashboard.UseVisualStyleBackColor = false;
+            buttonDashboard.Click += buttonDashboard_Click;
             // 
             // panel3
             // 
-            panel3.Controls.Add(panel4);
+            panel3.Controls.Add(panelSideButton);
             panel3.Dock = DockStyle.Left;
             panel3.Location = new Point(0, 133);
             panel3.Name = "panel3";
             panel3.Size = new Size(8, 587);
             panel3.TabIndex = 0;
             // 
-            // panel4
+            // panelSideButton
             // 
-            panel4.BackColor = Color.FromArgb(192, 0, 192);
-            panel4.Location = new Point(0, 0);
-            panel4.Name = "panel4";
-            panel4.Size = new Size(10, 125);
-            panel4.TabIndex = 2;
+            panelSideButton.BackColor = Color.FromArgb(192, 0, 192);
+            panelSideButton.Location = new Point(0, 66);
+            panelSideButton.Name = "panelSideButton";
+            panelSideButton.Size = new Size(10, 73);
+            panelSideButton.TabIndex = 2;
+            panelSideButton.Click += panelSideButton_Click;
+            panelSideButton.Paint += panelSideButton_Paint;
             // 
             // label1
             // 
@@ -291,7 +302,7 @@
             // 
             panelUp.Controls.Add(pictureBoxExpand);
             panelUp.Controls.Add(pictureBox2);
-            panelUp.Controls.Add(label_welcome);
+            panelUp.Controls.Add(labelTime);
             panelUp.Controls.Add(panelCek);
             panelUp.Dock = DockStyle.Top;
             panelUp.Location = new Point(229, 0);
@@ -311,6 +322,7 @@
             pictureBoxExpand.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBoxExpand.TabIndex = 4;
             pictureBoxExpand.TabStop = false;
+            pictureBoxExpand.Click += pictureBoxExpand_Click;
             // 
             // pictureBox2
             // 
@@ -323,22 +335,23 @@
             pictureBox2.TabIndex = 3;
             pictureBox2.TabStop = false;
             // 
-            // label_welcome
+            // labelTime
             // 
-            label_welcome.AutoSize = true;
-            label_welcome.Location = new Point(25, 24);
-            label_welcome.Name = "label_welcome";
-            label_welcome.Size = new Size(26, 20);
-            label_welcome.TabIndex = 2;
-            label_welcome.Text = "{?}";
+            labelTime.AutoSize = true;
+            labelTime.Location = new Point(25, 24);
+            labelTime.Name = "labelTime";
+            labelTime.Size = new Size(26, 20);
+            labelTime.TabIndex = 2;
+            labelTime.Text = "{?}";
+            labelTime.Click += labelTime_Click;
             // 
             // panelCek
             // 
             panelCek.BackColor = Color.Thistle;
             panelCek.Controls.Add(panelExpand);
-            panelCek.Controls.Add(label4);
+            panelCek.Controls.Add(labelRole);
             panelCek.Controls.Add(label5);
-            panelCek.Controls.Add(label3);
+            panelCek.Controls.Add(labelUsername);
             panelCek.Controls.Add(label2);
             panelCek.Dock = DockStyle.Bottom;
             panelCek.Location = new Point(0, 60);
@@ -358,6 +371,7 @@
             panelExpand.Name = "panelExpand";
             panelExpand.Size = new Size(215, 133);
             panelExpand.TabIndex = 0;
+            panelExpand.Paint += panelExpand_Paint;
             // 
             // button2
             // 
@@ -389,6 +403,7 @@
             button1.Text = "Minimize";
             button1.TextImageRelation = TextImageRelation.ImageBeforeText;
             button1.UseVisualStyleBackColor = true;
+            button1.Click += buttonMinimize_Click;
             // 
             // panel2
             // 
@@ -398,14 +413,14 @@
             panel2.Size = new Size(213, 5);
             panel2.TabIndex = 0;
             // 
-            // label4
+            // labelRole
             // 
-            label4.AutoSize = true;
-            label4.Location = new Point(123, 72);
-            label4.Name = "label4";
-            label4.Size = new Size(26, 20);
-            label4.TabIndex = 6;
-            label4.Text = "{?}";
+            labelRole.AutoSize = true;
+            labelRole.Location = new Point(123, 72);
+            labelRole.Name = "labelRole";
+            labelRole.Size = new Size(26, 20);
+            labelRole.TabIndex = 6;
+            labelRole.Text = "{?}";
             // 
             // label5
             // 
@@ -416,14 +431,14 @@
             label5.TabIndex = 5;
             label5.Text = "Role:";
             // 
-            // label3
+            // labelUsername
             // 
-            label3.AutoSize = true;
-            label3.Location = new Point(123, 32);
-            label3.Name = "label3";
-            label3.Size = new Size(26, 20);
-            label3.TabIndex = 4;
-            label3.Text = "{?}";
+            labelUsername.AutoSize = true;
+            labelUsername.Location = new Point(123, 32);
+            labelUsername.Name = "labelUsername";
+            labelUsername.Size = new Size(26, 20);
+            labelUsername.TabIndex = 4;
+            labelUsername.Text = "{?}";
             // 
             // label2
             // 
@@ -434,12 +449,26 @@
             label2.TabIndex = 3;
             label2.Text = "Welcome :";
             // 
+            // timerDateAndTime
+            // 
+            timerDateAndTime.Tick += timerDateAndTime_Tick;
+            // 
+            // userControlforDashboard1
+            // 
+            userControlforDashboard1.Anchor = AnchorStyles.None;
+            userControlforDashboard1.Location = new Point(270, 229);
+            userControlforDashboard1.Name = "userControlforDashboard1";
+            userControlforDashboard1.Size = new Size(905, 458);
+            userControlforDashboard1.TabIndex = 2;
+            userControlforDashboard1.Load += userControlforDashboard1_Load;
+            // 
             // FormMain
             // 
             AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.White;
             ClientSize = new Size(1200, 720);
+            Controls.Add(userControlforDashboard1);
             Controls.Add(panelUp);
             Controls.Add(panelSide);
             FormBorderStyle = FormBorderStyle.None;
@@ -478,10 +507,10 @@
         private Label label1;
         private Panel panelUp;
         private Panel panelCek;
-        private Label label_welcome;
-        private Label label4;
+        private Label labelTime;
+        private Label labelRole;
         private Label label5;
-        private Label label3;
+        private Label labelUsername;
         private Label label2;
         private PictureBox pictureBox2;
         private PictureBox pictureBoxExpand;
@@ -490,7 +519,7 @@
         private Panel panel2;
         private Button button2;
         private Panel panel3;
-        private Panel panel4;
+        private Panel panelSideButton;
         private Button buttonDashboard;
         private PictureBox pictureBoxRegister;
         private Button buttonRegister;
@@ -503,5 +532,7 @@
         private PictureBox pictureBoxAttendance;
         private Button buttonAttendance;
         private PictureBox pictureDashboard;
+        private System.Windows.Forms.Timer timerDateAndTime;
+        private Model.UserControlCheck.UserControlforDashboard userControlforDashboard1;
     }
 }
